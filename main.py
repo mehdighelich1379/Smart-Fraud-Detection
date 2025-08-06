573bc1330b17a97ebf758e9545ec8697a584505e
Mehdi Ghelich <qelejkhanimehdi@gmail.com>
2025-08-07 00:39:25 +0330

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    from src.models.train_model import training
-    from src.utils.metrics import results
-
-    pipeline, X, Y, x_test, y_test = training()
-    df_cv = results(pipeline, x_test, y_test)
