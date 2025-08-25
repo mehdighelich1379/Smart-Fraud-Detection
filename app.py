299b525576c25a02cd650464c509552474aa6992
Mehdi Ghelich <qelejkhanimehdi@gmail.com>
2025-08-25 15:09:16 +0330

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def set_background(image_file):
     st.markdown(css, unsafe_allow_html=True)
 
 # -------------- Load Model --------------
-pipeline = joblib.load("src/models/fraud_catboost_pipeline.pkl")
+pipeline = joblib.load("src/models/fraud_LGBMClassifier_pipeline")
 
 # -------------- Page Config --------------
 st.set_page_config(page_title="Fraud Detection App", layout="centered")
@@ -188,3 +188,4 @@ if st.button("🧠 Predict Fraud"):
 # -------------- Footer --------------
 st.markdown("---")
 st.markdown("Made with ❤️ using Streamlit and CatBoost")
+
